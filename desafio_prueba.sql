0f1691293dad8c9f8c7cf5bec0bfccb1e0f6e95c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-09-26 11:14:16 -0300

    cliente table was created with its content

--- desafio_prueba.sql
+++ desafio_prueba.sql
@@ -1,2 +1,8 @@
 CREATE DATABASE prueba;
 
+CREATE TABLE cliente(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    rut NUMERIC(10) NOT NULL,
+    direccion VARCHAR(100) NOT NULL
+);
